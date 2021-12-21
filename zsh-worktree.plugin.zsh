eb99baf31c0591952cb4c588ee18dab94b08336a
Alexis Zamanidis <alexiszamanidis@outlook.com>
2021-12-22 01:45:01 +0200

    add _wt_add and _wt_fetch

--- zsh-worktree.plugin.zsh
+++ zsh-worktree.plugin.zsh
@@ -4,6 +4,8 @@ _help() {
     echo "Usage:"
     echo -e "\twt list: List details of each working tree"
     echo -e "\twt prune: Prune working tree information"
+    echo -e "\twt fetch: Fetch branches from the bare repository"
+    echo -e "\twt add <worktree-name>: Create new working tree"
 }
 
 _wt_list() {
@@ -12,6 +14,22 @@ _wt_list() {
 
 _wt_prune() {
     git worktree prune
+}
+
+_wt_add() {
+    _wt_fetch
+
+    git worktree add $1
+
+    cd $1
+}
+
+_wt_fetch() {
+    if ! _move_to_bare_repo; then
+        return 0
+    fi
+
+    _bare_repo_fetch
 }
 
 # TODO: i need to revisit this method
@@ -49,5 +67,9 @@ wt() {
         _wt_list
     elif [ $1 = "prune" ]; then
         _wt_prune
+    elif [ $1 = "fetch" ]; then
+        _wt_fetch
+    elif [ $1 = "add" ] && [ $# -eq 2 ]; then
+        _wt_add $2
     fi
 }
